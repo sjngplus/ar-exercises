d25aeaddebb074e65be884ceff7316970ad475fc
SJ <sj.jega2@gmail.com>
2021-10-12 01:54:10 -0400

    complete exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+total_revenue = Store.sum('annual_revenue')
+puts total_revenue
+
+average_revenue = Store.average('annual_revenue')
+puts average_revenue
+
+puts Store.where(annual_revenue: 1000000..Float::INFINITY).count
